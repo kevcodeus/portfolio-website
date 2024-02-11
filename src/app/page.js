dd7fe7e58b10f840a43b5852ac1cc98717ae2b67
kevcodeus <126352240+kevcodeus@users.noreply.github.com>
2024-02-12 02:40:28 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
+import React,{useClient} from 
 
 export default function Home() {
+  import useClient;
   return (
     <main className="flex flex-col min-h-screen bg-[#121212]">
       <Navbar />
