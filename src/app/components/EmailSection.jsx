c5e2af709d4344a96b45672acc4d9457313bec91
kevcodeus <126352240+kevcodeus@users.noreply.github.com>
2024-02-12 01:10:11 +0530

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -53,10 +53,10 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let's Connect
+          Let&apos;s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md overflow-auto">
-          I'm actively seeking new opportunities and always eager to connect. Whether you have inquiries or simply want to say hello, feel free to reach out! I'll do my best to respond promptly.
+          I&apos;m actively seeking new opportunities and always eager to connect. Whether you have inquiries or simply want to say hello, feel free to reach out! I'll do my best to respond promptly.
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/kevcodeus">
