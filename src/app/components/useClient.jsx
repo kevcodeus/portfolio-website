fb26da08d0469713641ebbeac67d36f2088af533
kevcodeus <126352240+kevcodeus@users.noreply.github.com>
2024-02-12 22:52:53 +0530

    Add files via upload

---
+++ src/app/components/useClient.jsx
@@ -0,0 +1,14 @@
+// hooks/useClient.js
+import { useState, useEffect } from 'react';
+
+const useClient = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
+
+export default useClient;
