5839328fd47bfba1ed9c21011481016c88409cf5
kevcodeus <126352240+kevcodeus@users.noreply.github.com>
2024-02-12 19:27:42 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import HeroSection from "../components/HeroSection";
-import AboutSection from "../components/AboutSection";
-import ProjectsSection from "../components/ProjectsSection";
-import EmailSection from "../components/EmailSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
 
-const Home = () => {
+export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AchievementsSection />
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
       </div>
-      <AboutSection />
-      <ProjectsSection />
-      <EmailSection />
       <Footer />
     </main>
   );
 }
-
-export default Home;
